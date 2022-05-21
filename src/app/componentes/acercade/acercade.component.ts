c45a041520307b52c3d0ef2f0d906f818ae5e531
seba1974 <84405601+seba1974@users.noreply.github.com>
2022-05-21 18:03:11 -0300

    Antes de Login

--- src/app/componentes/acercade/acercade.component.ts
+++ src/app/componentes/acercade/acercade.component.ts
@@ -10,14 +10,22 @@ import { AcercadeService } from 'src/app/servicios/acercade.service';
 export class AcercadeComponent implements OnInit {
   listaAcercade:any; // variable 
 
-  constructor(private datosAcercade:AcercadeService) { }
+  constructor(private acercadeService:AcercadeService) { }
 
   ngOnInit(): void {
-    this.datosAcercade.obtenerAcercade().subscribe(data => {
+    this.acercadeService.obtenerAcercade().subscribe(data => {
       console.log(data);
       this.listaAcercade = data; //nombre de la tabla de la BD o JSON
       
     })
   }
+  editarAcercade(): void {
+    alert ("se va a editar Acerca de...");
+    //A RESOLVER  this.acercadeService.editarAcercade(this.listaAcercade.id, listaAcercade);
+  }
 
+  editarFoto(): void {
+    alert ("se va a editar la Foto...");
+  }
 }
+
--- src/app/servicios/acercade.service.ts
+++ src/app/servicios/acercade.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Acercade } from '../Models/acercade';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -15,22 +16,9 @@ export class AcercadeService {
 
   obtenerAcercade():Observable<Acercade[]>  {
   return this.http.get <Acercade[]>(this.acercadeURL + '/lista'); /* aca va la url de la bd*/
-} 
+  }
 
-borrarAcercade(id: number):Observable<any>  {
-  return this.http.delete <Acercade[]>(this.acercadeURL + '/borrar'); 
-}
-
-/*obtenerAcercade():Observable<any>  {
-  return this.http.get('./assets/data/porfolio.json'); /* funciona
-}*/
-/*crearAcercade(id: number, dni: String, ):Observable<any> {
-  return this.http.put <Acercade[]>(this.acercadeURL + '/crear');
-}
-
-
-modificarAcercade(acercade: Acercade):Observable<any>  {
-  return this.http.post<Acercade[]> (this.acercadeURL + '/modificar');
-
-}*/
+  editarAcercade(id:string, acercade : Acercade):Observable<any> {
+  return this.http.put(this.acercadeURL + '/modificar/' + id, acercade);
+  }
 }
