62e5395966d1850550aac5c9157e6697e41688ca
seba1974 <84405601+seba1974@users.noreply.github.com>
2022-07-28 15:15:02 -0300

    version 2.0.1

--- src/app/componentes/idiomas/idiomas.component.ts
+++ src/app/componentes/idiomas/idiomas.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IdiomasService } from 'src/app/servicios/idiomas.service';
 import { TokenService } from 'src/app/servicios/token.service';
+// declaro variable global google de tipo any
+declare var google: any;
 
 @Component({
   selector: 'app-idiomas',
@@ -32,11 +34,41 @@ export class IdiomasComponent implements OnInit {
     });
   }
 
-  eliminarIdiomas(id: string) {
-    this.idiomasService.borrarIdiomas(id).subscribe
-      (
-        data => { this.ngOnInit(); },
-        err => console.log(err)
-      );
+
+  crearGrafico(idioma: any, porcentaje_oral: any, porcentaje_escrita:any, porcentaje_lectura:any) {
+    console.log(idioma);
+    console.log(porcentaje_oral);
+    console.log(porcentaje_escrita);
+    console.log(porcentaje_lectura);
+
+
+      var func = (chart: any) => {
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Habilidad');
+      data.addColumn('number', 'Porcentaje Oral');
+      data.addColumn('number', 'Porcentaje Escrito');
+      data.addColumn('number', 'Porcentaje Lectura');
+      data.addRows([
+        ["Oral", parseInt(porcentaje_oral,10)],
+        ["Escritura", parseInt(porcentaje_escrita, 10)],
+        ["Lectura", parseInt(porcentaje_lectura, 10)]
+      ]);
+      var options = {
+        'title': idioma,
+        'width': 400,
+        'height': 300,
+        
+        'slices': {
+          0: { color: '#red' },
+          1: { color: '#blue' },
+          2: { color: 'yellow' }
+        }
+      };
+      chart().draw(data, options);
+    }
+
+    var chart = () => new google.visualization.PieChart(document.getElementById('chart_div'));
+    var callback = () => func(chart);
+    google.charts.setOnLoadCallback(callback);
   }
 }
